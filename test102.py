fcce5e3271f726d1e0ac0b0e19193786089586c0
Florian <raeuberhoehle2004@gmail.com>
2025-09-30 00:22:42 +0200

    code cleanup

--- test102.py
+++ test102.py
@@ -15,7 +15,8 @@ while True:
     while True:
         input_1 = input("Input first number: ")
         try:
-            input_1 = int(input_1)
+            input_1 = float(input_1)
+            if input_1.is_integer(): input_1 = int(input_1)
             break
         except:
             print("Invalid input! Ensure input is a number")
@@ -30,27 +31,23 @@ while True:
     while True:
         input_2 = input("Input second number: ")
         try:
-            input_2 = int(input_2)
+            input_2 = float(input_2)
+            if input_2.is_integer(): input_2 = int(input_2)
             break
         except:
             print("Invalid input! Ensure input is a number")
 
-
-    if operation == "+":
-        result = input_1 + input_2
-        print(f"{input_1} + {input_2} is equal to {result}")
-    elif operation == "-":
-        result = input_1 - input_2
-        print(f"{input_1} - {input_2} is equal to {result}")
-    elif operation == "X":
-        result = input_1 * input_2
-        print(f"{input_1} x {input_2} is equal to {result}")
+    if operation == "+": result = input_1 + input_2
+    elif operation == "-": result = input_1 - input_2
+    elif operation == "X": result = input_1 * input_2
     elif operation == "/":
         if input_2 == 0:
             print("Error! Division by zero")
             continue
         result = input_1 / input_2
-        print(f"{input_1} / {input_2} is equal to {result}")
+
+    if result.is_integer(): result = int(result)
+    print(f"{input_1} {operation} {input_2} is equal to {result}")
 
     while True:
         new_calculation_query = ""
