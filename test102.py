3234955649ddd48540fd9cd245c4d114178ad8b5
Florian <raeuberhoehle2004@gmail.com>
2025-09-30 00:34:59 +0200

    nvm that broke it

--- test102.py
+++ test102.py
@@ -15,7 +15,8 @@ while True:
     while True:
         input_1 = input("Input first number: ")
         try:
-            input_1 = int(input_1) if input_1.is_integer() else input_1 = float(input_1)
+            input_1 = float(input_1)
+            if input_1.is_integer(): input_1 = int(input_1)
             break
         except: print("Invalid input! Ensure input is a number")
 
@@ -27,7 +28,8 @@ while True:
     while True:
         input_2 = input("Input second number: ")
         try:
-            input_2 = int(input_2) if input_2.is_integer() else input_2 = float(input_2)
+            input_2 = float(input_2)
+            if input_2.is_integer(): input_2 = int(input_2)
             break
         except: print("Invalid input! Ensure input is a number")
 
