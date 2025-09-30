9961e5f89455fa28d4234b427875a2a7ec63da1a
Florian <raeuberhoehle2004@gmail.com>
2025-10-01 00:42:50 +0200

    actually did the thing i was meant to do

--- test104.py
+++ test104.py
@@ -3,8 +3,13 @@
 	•	Tell them if it’s even or odd.
 	•	Extend it: run continuously until the user types exit.'''
 
+number = input("Enter your number to check whether it is even or odd: ")
 while True:
-    number = input("Enter your number to check whether it is even or odd: ")
     try: number = int(number); break
+    except: number = input("\nInput is not a number! Try again: ")
+while True:
+    # print("Number is even.") if (number % 2) == 0 else print("Number is odd.")
+    number = input(f"Number is {"even" if (number % 2) == 0 else "odd"}. Do you want to continue? Enter another number or enter 'exit' to exit: ")
+    if number.lower() == "exit": exit()
+    try: number = int(number)
     except: input("\nInput is not a number! Try again: ")
-print("Number is even.") if (number % 2) == 0 else print("Number is odd.")
