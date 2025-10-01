fe5d6e354cb04722cbda100971ef6d6808ef6bfd
Florian <raeuberhoehle2004@gmail.com>
2025-10-01 22:13:02 +0200

    word reverser done

--- projects/11-word-reverser.py
+++ projects/11-word-reverser.py
@@ -6,3 +6,11 @@ Print the word backwards.
 
 Extend it: keep asking for words until the user types “stop”.'''
 
+while True:
+    word = input("Enter a word you want to reverse or type stop to exit: ")
+    while True:
+        try: int(word)
+        except:
+            if word.lower() == "stop": exit()
+            else: break
+    print(f'"{word}" reversed spells "{word[::-1]}"!')
