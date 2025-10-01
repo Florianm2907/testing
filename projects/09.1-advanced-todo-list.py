891d447978ee0e1383ed6e0c1943b667de2a7f6a
Florian <raeuberhoehle2004@gmail.com>
2025-10-01 16:17:12 +0200

    changed description to match actual usability

--- projects/09.1-advanced-todo-list.py
+++ projects/09.1-advanced-todo-list.py
@@ -1,7 +1,6 @@
 '''9. Basic To-Do List (text only)
 	•	Let the user add, remove, and view tasks in a list.
-	•	Keep looping until they type quit.
-	•	(You don’t need to save to a file yet — just use variables).'''
+	•	Keep looping until they type quit.'''
 
 todo = []
 
