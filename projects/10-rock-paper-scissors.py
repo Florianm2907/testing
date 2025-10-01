9e17fe518a3076f014cddc015eaa7cfc61d1c3be
Florian <raeuberhoehle2004@gmail.com>
2025-10-01 02:29:02 +0200

    test 10

---
+++ projects/10-rock-paper-scissors.py
@@ -0,0 +1,6 @@
+'''10. Rock, Paper, Scissors
+	•	Ask user to pick rock/paper/scissors.
+	•	Computer randomly picks one.
+	•	Compare and announce winner.
+	•	Play multiple rounds until the user quits.'''
+
