859871f02435ba95adb80f4b42b7378077d118fb
Florian <raeuberhoehle2004@gmail.com>
2025-10-01 13:12:58 +0200

    todo list

---
+++ projects/09-basic-todo-list.py
@@ -0,0 +1,4 @@
+'''9. Basic To-Do List (text only)
+	•	Let the user add, remove, and view tasks in a list.
+	•	Keep looping until they type quit.
+	•	(You don’t need to save to a file yet — just use variables).'''
